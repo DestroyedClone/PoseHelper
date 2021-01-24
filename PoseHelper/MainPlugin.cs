2df6a7dc36e0aab482c841c7978686bc57c07406
DestroyedClone <DestroyedClone@gmail.com>
2021-01-24 01:53:43 -0800

    Add project files.

---
+++ PoseHelper/MainPlugin.cs
@@ -0,0 +1,20 @@
+﻿using BepInEx;
+using R2API.Utils;
+
+namespace PoseHelper
+{
+    public class MainPlugin : BaseUnityPlugin
+    {
+        public const string ModVer = "1.0.0";
+        public const string ModName = "Pose Helper";
+        public const string ModGuid = "com.DestroyedClone.PoseHelper";
+
+        internal static BepInEx.Logging.ManualLogSource _logger;
+
+        private void Awake()
+        {
+            _logger = Logger;
+            CommandHelper.AddToConsoleWhenReady();
+        }
+    }
+}
