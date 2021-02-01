bffd4a809863f8886604610f59df09cd6a5f885d
DestroyedClone <DestroyedClone@gmail.com>
2021-01-31 22:07:57 -0800

    dsadsad

--- MoonToOutro/MTOPlugin.cs
+++ MoonToOutro/MTOPlugin.cs
@@ -1,22 +1,37 @@
 ﻿using BepInEx;
+using R2API.Utils;
 using RoR2;
+using UnityEngine;
 
 namespace MoonToOutro
 {
     [BepInPlugin("com.DestroyedClone.MoonToOutro", "Immediate Moon To Outro", "1.0.0")]
+    [R2APISubmoduleDependency(nameof(CommandHelper))]
     [BepInDependency(R2API.R2API.PluginGUID, R2API.R2API.PluginVersion)]
     public class Plugin : BaseUnityPlugin
     {
         public void Awake()
         {
+            R2API.Utils.CommandHelper.AddToConsoleWhenReady();
             On.RoR2.SceneDirector.Start += SceneDirector_Start;
         }
 
         private void SceneDirector_Start(On.RoR2.SceneDirector.orig_Start orig, SceneDirector self)
         {
             orig(self);
-            if (UnityEngine.SceneManagement.SceneManager.GetActiveScene().name == "moon")
+            if (UnityEngine.SceneManagement.SceneManager.GetActiveScene().name == "moon" && PlayerCharacterMasterController.instances[0].master?.gameObject.GetComponent<ApprovedToSkipOutro>())
                 UnityEngine.Object.FindObjectOfType<EscapeSequenceController>().CompleteEscapeSequence();
+        }
+
+        [ConCommand(commandName = "skipmoon", flags = ConVarFlags.ExecuteOnServer, helpText = "Immediately completes Commencement to properly head to the outro quickly. Must be used mid-run.")]
+        private static void MyCommandName(ConCommandArgs args)
+        {
+            args.senderMasterObject.AddComponent<ApprovedToSkipOutro>();
+            RoR2.Console.instance.SubmitCmd(args.sender, "set_scene moon", false);
+        }
+        public class ApprovedToSkipOutro : MonoBehaviour
+        {
+
         }
     }
 }
---
+++ MoonToOutro/README.md
@@ -0,0 +1,4 @@
+﻿# Moon to Outro
+Basically used just for seeing your outro flavortext. You need to be in-game for this to work. You can just start a new run then immediately run the command.
+
+Use "**skipmoon**" in console to complete the escape sequence and auto progress to the outro scene.
