21b91b0fd2f8c1e97fa011d35cb7c8c0a349c9a6
DestroyedClone <DestroyedClone@gmail.com>
2021-02-27 22:45:43 -0800

    actually organizing this damn file lol

--- LobbyAppearanceImprovements/Class1.cs
+++ LobbyAppearanceImprovements/Class1.cs
@@ -60,31 +60,6 @@ namespace LobbyAppearanceImprovements
         public static ConfigEntry<bool> SurvivorsInLobby { get; set; }
         public static ConfigEntry<int> SelectViewMode { get; set; }
 
-        public Dictionary<string, float[]> textCameraSettings = new Dictionary<string, float[]>
-        {
-            {"Commando", new float[]{ 20, 2, 24 } },
-            {"Huntress", new float[]{ 9, -3, 18 } },
-            {"Toolbot", new float[]{ 10, -1, -1 } },
-            {"Engi", new float[]{ 6, 1, -7.5f } },
-            {"Mage", new float[]{ 8, -1, 13 } },
-            {"Merc", new float[]{ 5, -8.5f, -3 } },
-            {"Treebot", new float[]{ 6, 0.7f, -15.5f } },
-            {"Loader", new float[]{ 11, 0, 20 } },
-            {"Croco", new float[]{ 8, -8.5f, 13 } },
-            {"Captain", new float[]{ 8, 0, 7 } },
-            {"SniperClassic", new float[]{ 6, 0.5f, -12.5f } },
-            {"Enforcer", new float[]{ 11, -1, 10 } },
-            {"NemesisEnforcer", new float[]{ 10, -7.5f, 8 } },
-            {"BanditReloaded", new float[]{ 20, 1, -30 } },
-            {"HANDOverclocked", new float[]{ 8, -2, -4 } },
-            {"Miner", new float[]{ 17, 1, -26 } },
-            {"RobPaladin", new float[]{ 9, -1, -10 } },
-            {"CHEF", new float[]{ 5, -8.5f, 3 } },
-            {"RobHenry", new float[]{ 12, -7, -27 } },
-            {"Wyatt", new float[]{ 12, -2, -22 } },
-            {"Executioner", new float[]{ 10, -1, 5 } },
-        };
-
         public Dictionary<SurvivorIndex, float[]> characterCameraSettings = new Dictionary<SurvivorIndex, float[]>();
 
         bool hasSetupCameraValues = false;
@@ -412,7 +387,7 @@ namespace LobbyAppearanceImprovements
                 if (!hasSetupCameraValues)
                 {
                     SurvivorIndex survivorIndex = survivorDef.survivorIndex;
-                    textCameraSettings.TryGetValue(bodyPrefabName, out float[] cameraSetting);
+                    StaticValues.textCameraSettings.TryGetValue(bodyPrefabName, out float[] cameraSetting);
                     if (!characterCameraSettings.ContainsKey(survivorIndex))
                         characterCameraSettings.Add(survivorIndex, cameraSetting);
                 }
---
+++ LobbyAppearanceImprovements/StaticValues.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LobbyAppearanceImprovements
+{
+    public static class StaticValues
+    {
+        public static Dictionary<string, float[]> textCameraSettings = new Dictionary<string, float[]>
+        {
+            {"Commando", new float[]{ 20, 2, 24 } },
+            {"Huntress", new float[]{ 9, -3, 18 } },
+            {"Toolbot", new float[]{ 10, -1, -1 } },
+            {"Engi", new float[]{ 6, 1, -7.5f } },
+            {"Mage", new float[]{ 8, -1, 13 } },
+            {"Merc", new float[]{ 5, -8.5f, -3 } },
+            {"Treebot", new float[]{ 6, 0.7f, -15.5f } },
+            {"Loader", new float[]{ 11, 0, 20 } },
+            {"Croco", new float[]{ 8, -8.5f, 13 } },
+            {"Captain", new float[]{ 8, 0, 7 } },
+            {"SniperClassic", new float[]{ 6, 0.5f, -12.5f } },
+            {"Enforcer", new float[]{ 11, -1, 10 } },
+            {"NemesisEnforcer", new float[]{ 10, -7.5f, 8 } },
+            {"BanditReloaded", new float[]{ 20, 1, -30 } },
+            {"HANDOverclocked", new float[]{ 8, -2, -4 } },
+            {"Miner", new float[]{ 17, 1, -26 } },
+            {"RobPaladin", new float[]{ 9, -1, -10 } },
+            {"CHEF", new float[]{ 5, -8.5f, 3 } },
+            {"RobHenry", new float[]{ 12, -7, -27 } },
+            {"Wyatt", new float[]{ 12, -2, -22 } },
+            {"Executioner", new float[]{ 10, -1, 5 } },
+        };
+    }
+}
