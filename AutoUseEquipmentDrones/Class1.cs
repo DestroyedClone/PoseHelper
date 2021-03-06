c8c0502d5970312e430eb363bd4279fd0f9c0c42
DestroyedClone <DestroyedClone@gmail.com>
2021-03-06 10:19:19 -0800

    new mod

---
+++ AutoUseEquipmentDrones/Class1.cs
@@ -0,0 +1,30 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using UnityEngine;
+using RoR2;
+using R2API.Utils;
+
+namespace AutoUseEquipmentDrones
+{
+    [BepInPlugin("com.DestroyedClone.BetterEquipmentDroneUse", "Better Equipment Drone Use", "1.0.0")]
+    [BepInDependency(R2API.R2API.PluginGUID, R2API.R2API.PluginVersion)]
+    [NetworkCompatibility(CompatibilityLevel.EveryoneMustHaveMod, VersionStrictness.EveryoneNeedSameModVersion)]
+    public class AUEDPlugin : BaseUnityPlugin
+    {
+
+        public void Awake()
+        {
+            On.RoR2.EquipmentSlot.FixedUpdate += EquipmentSlot_FixedUpdate;
+        }
+
+        private void EquipmentSlot_FixedUpdate(On.RoR2.EquipmentSlot.orig_FixedUpdate orig, EquipmentSlot self)
+        {
+            orig(self);
+            switch (self.equipmentIndex)
+            {
+                case EquipmentIndex.CommandMissile:
+                    break;
+            }
+        }
+    }
+}
