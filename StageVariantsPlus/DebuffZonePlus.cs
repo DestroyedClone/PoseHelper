9b330e3c2c1019bef3ed7b822e32a7851793102b
DestroyedClone <DestroyedClone@gmail.com>
2021-03-19 14:28:09 -0700

    added goolake modify

---
+++ StageVariantsPlus/DebuffZonePlus.cs
@@ -0,0 +1,69 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using UnityEngine;
+using RoR2;
+using R2API.Utils;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+
+namespace StageVariantsPlus
+{
+    public class DebuffZonePlus : MonoBehaviour
+    {
+        [Tooltip("The buff type to grant")]
+        public BuffIndex buffType;
+        [Tooltip("The buff duration")]
+        public float buffDuration;
+        public string buffApplicationSoundString;
+        public GameObject buffApplicationEffectPrefab;
+
+        private List<Collider> previousColliderList = new List<Collider>();
+        private float resetStopwatch = 3f;
+        public float orbResetListFrequency;
+
+
+        public void FixedUpdate()
+        {
+            if (this.previousColliderList.Count > 0)
+            {
+                this.resetStopwatch += Time.fixedDeltaTime;
+                if (this.resetStopwatch > 1f / this.orbResetListFrequency)
+                {
+                    this.resetStopwatch -= 1f / this.orbResetListFrequency;
+                    this.previousColliderList.Clear();
+                }
+            }
+        }
+
+        public void OnTriggerStay(Collider other)
+        {
+            if (NetworkServer.active)
+            {
+                if (this.previousColliderList.Contains(other))
+                {
+                    return;
+                }
+                this.previousColliderList.Add(other);
+                CharacterBody component = other.GetComponent<CharacterBody>();
+                if (component && component.mainHurtBox)
+                {
+                    ApplyDebuff(component);
+                }
+            }
+        }
+
+        private void ApplyDebuff(CharacterBody characterBody)
+        {
+            characterBody.AddTimedBuff(this.buffType, this.buffDuration);
+            Util.PlaySound(this.buffApplicationSoundString, characterBody.gameObject);
+            if (this.buffApplicationEffectPrefab)
+            {
+                EffectManager.SpawnEffect(this.buffApplicationEffectPrefab, new EffectData
+                {
+                    origin = characterBody.mainHurtBox.transform.position,
+                    scale = characterBody.radius
+                }, true);
+            }
+        }
+    }
+}
--- StageVariantsPlus/SVPlugin.cs
+++ StageVariantsPlus/SVPlugin.cs
@@ -3,8 +3,10 @@ using BepInEx.Configuration;
 using UnityEngine;
 using RoR2;
 using R2API.Utils;
+using System.Collections.Generic;
+using UnityEngine.Networking;
 
-namespace StageVariants
+namespace StageVariantsPlus
 {
     [BepInPlugin("com.DestroyedClone.StageVariantsPlus", "Stage Variants Plus", "1.0.0")]
     [BepInDependency(R2API.R2API.PluginGUID, R2API.R2API.PluginVersion)]
@@ -19,6 +21,35 @@ namespace StageVariants
         private void ChooseSceneToModify(On.RoR2.SceneDirector.orig_Start orig, SceneDirector self)
         {
             orig(self);
+            if (UnityEngine.SceneManagement.SceneManager.GetActiveScene().name == "goolake")
+            {
+                if (Util.CheckRoll(100))
+                {
+                    ModifyGooLake();
+                }
+            }
         }
+
+        private void ModifyGooLake()
+        {
+            var miscprops = GameObject.Find("HOLDER: Misc Props");
+            var gooplane = miscprops.transform.Find("GooPlane");
+            var gooplanehigh = miscprops.transform.Find("GooPlane, High");
+            var debuffArea = gooplane.transform.Find("DEBUFF ZONE: Plane");
+            var SFX = gooplane.transform.Find("SFX");
+
+            var debuffClone = Instantiate<GameObject>(debuffArea.gameObject, gooplanehigh);
+            var SFXClone = Instantiate<GameObject>(SFX.gameObject, gooplanehigh);
+
+            var debuffZone = debuffClone.GetComponent<DebuffZone>();
+            var debuffZonePlus = debuffClone.AddComponent<DebuffZonePlus>();
+            debuffZonePlus.buffApplicationEffectPrefab = debuffZone.buffApplicationEffectPrefab;
+            debuffZonePlus.buffApplicationSoundString = debuffZone.buffApplicationSoundString;
+            debuffZonePlus.buffDuration = debuffZone.buffDuration;
+            debuffZonePlus.buffType = debuffZone.buffType;
+            debuffZonePlus.orbResetListFrequency = debuffZonePlus.buffDuration;
+            UnityEngine.Object.Destroy(debuffZone);
+        }
+
     }
 }
