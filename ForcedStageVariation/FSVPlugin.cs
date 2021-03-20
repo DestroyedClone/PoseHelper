89d6eef20f9b71ef50c18faca837675e5f024895
DestroyedClone <DestroyedClone@gmail.com>
2021-03-19 17:37:49 -0700

    setup config

--- ForcedStageVariation/FSVPlugin.cs
+++ ForcedStageVariation/FSVPlugin.cs
@@ -9,7 +9,7 @@ namespace ForcedStageVariation
     [BepInPlugin("com.DestroyedClone.ForcedStageVariation", "Forced Stage Variation", "1.0.0")]
     [BepInDependency(R2API.R2API.PluginGUID, R2API.R2API.PluginVersion)]
     [NetworkCompatibility(CompatibilityLevel.NoNeedForSync, VersionStrictness.DifferentModVersionsAreOk)]
-    public class FSVPlugin
+    public class FSVPlugin : BaseUnityPlugin
     {
         public static ConfigEntry<bool> rootJungleTreasureChests { get; set; }
         public static ConfigEntry<bool> rootJungleTunnelLandmass { get; set; }
@@ -18,7 +18,25 @@ namespace ForcedStageVariation
 
         public void Awake()
         {
+            rootJungleTreasureChests = Config.Bind("Sundred Grove", "Treasure Chest Location", true, "-1 = Default" +
+                "\n0 = Root Bridge Front Chest" +
+                "\n1 = Mushroom Cave Chest" +
+                "\n2 = Treehouse Hole" +
+                "\n3 = Triangle Cave" +
+                "\n4 = Downed Tree Roots");
+            rootJungleTunnelLandmass = Config.Bind("Sundred Grove", "Tunnel Landmass", true, "-1 = Default" +
+                "\n0 = Enabled" +
+                "\n1 = No Tunnel Landmass");
+            rootJungleHeldRocks = Config.Bind("Sundred Grove", "Held Rocks", true, "-1 = Default" +
+                "\n0 = Held Rock" +
+                "\n1 = Split Rock");
+            rootJungleUndergroundShortcut = Config.Bind("Sundred Grove", "Underground Shortcut", true, "-1 = Default" +
+                "\n0 = Open" +
+                "\n1 = Closed");
+
 
         }
+
+
     }
 }
