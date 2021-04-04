9cded671d5f71b255d332715e91a908a2cf2b906
DestroyedClone <DestroyedClone@gmail.com>
2021-04-04 10:52:51 -0700

    publicized

--- ForcedStageVariation/FSVPlugin.cs
+++ ForcedStageVariation/FSVPlugin.cs
@@ -3,7 +3,13 @@ using BepInEx.Configuration;
 using UnityEngine;
 using RoR2;
 using UnityEngine.Networking;
+using System.Security;
+using System.Security.Permissions;
 //using R2API.Utils;
+[module: UnverifiableCode]
+#pragma warning disable CS0618 // Type or member is obsolete
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+#pragma warning restore CS0618 // Type or member is obsolete
 
 namespace ForcedStageVariation
 {
