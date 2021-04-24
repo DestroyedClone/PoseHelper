91b648f0d8d1d8c3cfc3107e50b629194127cc41
DestroyedClone <DestroyedClone@gmail.com>
2021-04-24 00:51:37 -0700

    awwaowoawoawo

---
+++ LobbyAppearanceImprovements/Plugin.cs
@@ -0,0 +1,59 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using LeTai.Asset.TranslucentImage;
+using R2API.Utils;
+using RoR2;
+using System.Collections.Generic;
+using System.Security;
+using System.Security.Permissions;
+using UnityEngine;
+using static UnityEngine.ColorUtility;
+using static LobbyAppearanceImprovements.StaticValues;
+using static LobbyAppearanceImprovements.Methods;
+
+namespace LobbyAppearanceImprovements
+{
+    public class Plugin : BaseUnityPlugin
+    {
+        public const string ModVer = "1.1.0";
+        public const string ModName = "LobbyAppearanceImprovements";
+        public const string ModGuid = "com.DestroyedClone.LobbyAppearanceImprovements";
+
+        // CONFIG //
+        // Ordered by Layer //
+        // UI //
+        public static ConfigEntry<bool> HideFade { get; set; }
+        public static ConfigEntry<int> BlurValue { get; set; }
+        public static ConfigEntry<float> UIScale { get; set; }
+
+        // Overlay //
+        // Anything that affects the scene as a whole //
+        // Post Processing //
+        public static ConfigEntry<bool> PostProcessing { get; set; }
+
+        // Lights //
+        // The primary light over the scene //
+        public static ConfigEntry<string> Light_Color { get; set; }
+        public static ConfigEntry<bool> Light_Flicker_Disable { get; set; }
+        public static ConfigEntry<float> Light_Intensity { get; set; }
+
+        // Character Pad Displays //
+        public static ConfigEntry<float> CharacterPadScale { get; set; }
+
+        // Background Elements //
+        // Anything in the background unrelated to the characters //
+        public static ConfigEntry<bool> MeshProps { get; set; }
+        public static ConfigEntry<bool> PhysicsProps { get; set; }
+        public static ConfigEntry<bool> DisableShaking { get; set; }
+
+        // Custom Background //
+        public static ConfigEntry<int> SelectedScene { get; set; }
+
+        // Survivors In Lobby //
+        // Anything related to the config setting to show displays in the lobby //
+        public static ConfigEntry<bool> SurvivorsInLobby { get; set; }
+        public static ConfigEntry<int> SelectViewMode { get; set; }
+        public static ConfigEntry<bool> ReplayAnim { get; set; }
+        public static ConfigEntry<bool> LivePreview { get; set; }
+    }
+}
