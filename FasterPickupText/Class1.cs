52e44369bda4203cda49af256c3b8ae7bcea2092
DestroyedClone <destroyedclone@gmail.com>
2021-08-07 03:49:02 -0700

    created base of mod

--- FasterPickupText/Class1.cs
+++ FasterPickupText/Class1.cs
@@ -1,9 +1,11 @@
 ﻿using BepInEx;
 using UnityEngine;
 using RoR2;
+using RoR2.UI;
 using R2API.Utils;
 using System.Security;
 using System.Security.Permissions;
+using System.Linq;
 using System.Collections.Generic;
 using UnityEngine.Networking;
 
@@ -17,11 +19,18 @@ namespace FasterPickupText
     [BepInPlugin("com.DestroyedClone.FasterPickupText", "Faster Pickup Text", "1.0.0")]
     [BepInDependency(R2API.R2API.PluginGUID, R2API.R2API.PluginVersion)]
     [NetworkCompatibility(CompatibilityLevel.NoNeedForSync, VersionStrictness.DifferentModVersionsAreOk)]
-    public class Class1
+    public class FasterPickupTextPlugin : BaseUnityPlugin
     {
         public void Awake()
         {
+            On.RoR2.UI.NotificationQueue.OnPickup += NotificationQueue_OnPickup;
+        }
 
+        private void NotificationQueue_OnPickup(On.RoR2.UI.NotificationQueue.orig_OnPickup orig, RoR2.UI.NotificationQueue self, CharacterMaster characterMaster, PickupIndex pickupIndex)
+        {
+            orig(self, characterMaster, pickupIndex);
+            if (self.currentNotification)
+                self.currentNotification.duration = 0;
         }
     }
 }
