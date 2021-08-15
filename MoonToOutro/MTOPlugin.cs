0326acce7182ac649c152a3a47283112e3477b2b
DestroyedClone <destroyedclone@gmail.com>
2021-08-14 19:00:47 -0700

    setup for v2

--- MoonToOutro/MTOPlugin.cs
+++ MoonToOutro/MTOPlugin.cs
@@ -15,13 +15,27 @@ namespace MoonToOutro
         {
             R2API.Utils.CommandHelper.AddToConsoleWhenReady();
             On.RoR2.SceneDirector.Start += SceneDirector_Start;
+            On.RoR2.UI.OutroFlavorTextController.UpdateFlavorText += OutroFlavorTextController_UpdateFlavorText;
+        }
+
+        private void OutroFlavorTextController_UpdateFlavorText(On.RoR2.UI.OutroFlavorTextController.orig_UpdateFlavorText orig, RoR2.UI.OutroFlavorTextController self)
+        {
+            orig(self);
+
         }
 
         private void SceneDirector_Start(On.RoR2.SceneDirector.orig_Start orig, SceneDirector self)
         {
             orig(self);
-            if (UnityEngine.SceneManagement.SceneManager.GetActiveScene().name == "moon" && PlayerCharacterMasterController.instances[0].master?.gameObject.GetComponent<ApprovedToSkipOutro>())
-                UnityEngine.Object.FindObjectOfType<EscapeSequenceController>().CompleteEscapeSequence();
+            if (PlayerCharacterMasterController.instances[0].master?.gameObject.GetComponent<ApprovedToSkipOutro>())
+            {
+                if (UnityEngine.SceneManagement.SceneManager.GetActiveScene().name == "moon")
+                    UnityEngine.Object.FindObjectOfType<EscapeSequenceController>().CompleteEscapeSequence();
+                if (UnityEngine.SceneManagement.SceneManager.GetActiveScene().name == "moon2")
+                {
+
+                }
+            }
         }
 
         [ConCommand(commandName = "skipmoon_old", flags = ConVarFlags.ExecuteOnServer, helpText = "Immediately completes Commencement to properly head to the outro quickly. Must be used mid-run.")]
@@ -34,20 +48,26 @@ namespace MoonToOutro
         [ConCommand(commandName = "skipmoon_win", flags = ConVarFlags.ExecuteOnServer, helpText = "Beats the game with a win.")]
         private static void SkipMoonWin(ConCommandArgs args)
         {
-            args.senderMasterObject.AddComponent<ApprovedToSkipOutro>();
-            RoR2.Console.instance.SubmitCmd(args.sender, "next_scene moon", false);
+            args.senderMasterObject.AddComponent<ApprovedToSkipOutro>().gameEndType = GameEndType.Win;
+            RoR2.Console.instance.SubmitCmd(args.sender, "next_scene moon2", false);
         }
 
         [ConCommand(commandName = "skipmoon_fail", flags = ConVarFlags.ExecuteOnServer, helpText = "Ends the game with a fail.")]
         private static void SkipMoonFail(ConCommandArgs args)
         {
-            args.senderMasterObject.AddComponent<ApprovedToSkipOutro>();
-            RoR2.Console.instance.SubmitCmd(args.sender, "next_scene moon", false);
+            args.senderMasterObject.AddComponent<ApprovedToSkipOutro>().gameEndType = GameEndType.Fail;
+            RoR2.Console.instance.SubmitCmd(args.sender, "next_scene moon2", false);
         }
 
         public class ApprovedToSkipOutro : MonoBehaviour
         {
+            public GameEndType gameEndType = GameEndType.Win;
+        }
 
+        public enum GameEndType
+        {
+            Win,
+            Fail
         }
     }
 }
