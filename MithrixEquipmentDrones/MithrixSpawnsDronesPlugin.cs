8012c0588c22615892327e942a4852966e0eaca3
DestroyedClone <destroyedclone@gmail.com>
2021-09-22 23:35:45 -0700

    zzz

--- MithrixEquipmentDrones/MithrixSpawnsDronesPlugin.cs
+++ MithrixEquipmentDrones/MithrixSpawnsDronesPlugin.cs
@@ -5,6 +5,9 @@ using System.Security;
 using System.Security.Permissions;
 using UnityEngine;
 using UnityEngine.Networking;
+using System.Collections.Generic;
+using RoR2.UI;
+using BepInEx.Configuration;
 
 [module: UnverifiableCode]
 #pragma warning disable CS0618 // Type or member is obsolete
@@ -13,11 +16,12 @@ using UnityEngine.Networking;
 
 namespace MithrixEquipmentDrones
 {
-    [BepInPlugin("com.DestroyedClone.MithrixSpawnsEquipmentDrones", "Mithrix Spawns Equipment Drones", "1.0.0")]
+    [BepInPlugin("com.DestroyedClone.MithrixSpawnsEquipmentDrones", "Mithrix Spawns Equipment Drones", "1.0.1")]
     [BepInDependency(R2API.R2API.PluginGUID, R2API.R2API.PluginVersion)]
     [NetworkCompatibility(CompatibilityLevel.EveryoneMustHaveMod, VersionStrictness.DifferentModVersionsAreOk)]
     public class MithrixSpawnsDronesPlugin : BaseUnityPlugin
     {
+
         public void Awake()
         {
             On.RoR2.ReturnStolenItemsOnGettingHit.Awake += ReturnStolenItemsOnGettingHit_Awake;
@@ -35,6 +39,7 @@ namespace MithrixEquipmentDrones
             public ReturnStolenItemsOnGettingHit returnStolenItems;
             private ItemStealController itemStealController;
             private GameObject masterPrefab = null;
+            List<EquipmentIndex> equipmentIndexes = new List<EquipmentIndex>();
 
             public void Start()
             {
@@ -58,27 +63,64 @@ namespace MithrixEquipmentDrones
                 }
             }
 
+            private void GetEquipmentDefs()
+            {
+                equipmentIndexes.Clear();
+                foreach (var pcmc in PlayerCharacterMasterController.instances)
+                {
+                    if (pcmc.master && pcmc.master.inventory && !pcmc.master.IsDeadAndOutOfLivesServer())
+                    {
+                        foreach (var equipmentState in pcmc.master.inventory.equipmentStateSlots)
+                        {
+                            if (equipmentState.equipmentDef != null)
+                            {
+                                equipmentIndexes.Add(equipmentState.equipmentIndex);
+                            }
+                        }
+                    }
+                }
+                if (equipmentIndexes.Count == 0)
+                    equipmentIndexes.Add(RoR2Content.Equipment.Fruit.equipmentIndex);
+            }
+
+            private void AdjustHealth(CharacterMaster characterMaster)
+            {
+                float num = 1f;
+                float num2 = 1f;
+                num += Run.instance.difficultyCoefficient / 2.5f;
+                num2 += Run.instance.difficultyCoefficient / 30f;
+                int num3 = Mathf.Max(1, Run.instance.livingPlayerCount);
+                num *= Mathf.Pow((float)num3, 0.5f);
+                Debug.LogFormat("Scripted Combat Encounter (Equipment Drone): currentBoostHpCoefficient={0}, currentBoostDamageCoefficient={1}", new object[]
+                {
+                        num,
+                        num2
+                });
+                characterMaster.inventory.GiveItem(RoR2Content.Items.BoostHp, Mathf.RoundToInt((num - 1f) * 10f));
+                characterMaster.inventory.GiveItem(RoR2Content.Items.BoostDamage, Mathf.RoundToInt((num2 - 1f) * 10f));
+                characterMaster.inventory.GiveItem(RoR2Content.Items.Hoof, 10);
+            }
+
             [Server]
             private void SpawnDronesForEachPlayer()
             {
-                int participatingPlayerCount = Run.instance.participatingPlayerCount != 0 ? Run.instance.participatingPlayerCount : 1;
+                GetEquipmentDefs();
+                int participatingPlayerCount = equipmentIndexes.Count;
                 float angle = 360f / participatingPlayerCount;
                 Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.up);
                 var nextPosition = gameObject.transform.position + Vector3.up * 8f + Vector3.forward * 8f;
 
                 int i = 0;
 
-                foreach (var playerCharacterMasterController in PlayerCharacterMasterController.instances)
+                RoR2.UI.HUDBossHealthBarController bossHealthBarController = UnityEngine.Object.FindObjectOfType<HUDBossHealthBarController>();
+
+                foreach (var equipmentIndex in equipmentIndexes)
                 {
-                    if (playerCharacterMasterController && playerCharacterMasterController.body && playerCharacterMasterController.body.healthComponent.alive)
-                    {
-                        EquipmentIndex equipmentIndex = RoR2Content.Equipment.Fruit.equipmentIndex;
-                        if (playerCharacterMasterController.body.inventory && playerCharacterMasterController.body.inventory.currentEquipmentState.equipmentIndex != EquipmentIndex.None)
-                            equipmentIndex = playerCharacterMasterController.body.inventory.GetEquipmentIndex();
-                        SummonDrone(gameObject, nextPosition, equipmentIndex);
-                        i++;
-                        nextPosition = rotation * nextPosition;
-                    }
+                    var drone = SummonDrone(gameObject, nextPosition, equipmentIndex);
+                    AdjustHealth(drone);
+                    bossHealthBarController.currentBossGroup.AddBossMemory(drone);
+                    i++;
+                    nextPosition = rotation * nextPosition;
                 }
             }
 
