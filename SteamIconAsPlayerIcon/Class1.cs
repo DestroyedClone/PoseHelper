246686ff07a230045314662cbbc5845bb848fe50
DestroyedClone <destroyedclone@gmail.com>
2021-09-29 12:54:40 -0700

    abandoned

---
+++ SteamIconAsPlayerIcon/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using BepInEx;
+using RoR2;
+using UnityEngine;
+
+
+namespace SteamIconAsPlayerIcon
+{
+    [BepInPlugin("com.DestroyedClone.SteamIconAsPlayerIcon", "Steam Icon As Player Icon", "1.0.0")]
+    public class Class1 : BaseUnityPlugin
+    {
+        public void Awake()
+        {
+            On.RoR2.UI.ScoreboardStrip.FindMasterPortrait += ScoreboardStrip_FindMasterPortrait;
+        }
+
+        private UnityEngine.Texture ScoreboardStrip_FindMasterPortrait(On.RoR2.UI.ScoreboardStrip.orig_FindMasterPortrait orig, RoR2.UI.ScoreboardStrip self)
+        {
+			if (self.userBody)
+			{
+				return self.userBody.portraitIcon;
+			}
+			if (self.master)
+			{
+				GameObject bodyPrefab = self.master.bodyPrefab;
+				if (bodyPrefab)
+				{
+					CharacterBody component = bodyPrefab.GetComponent<CharacterBody>();
+					if (component)
+					{
+						return component.portraitIcon;
+					}
+				}
+			}
+			return null;
+		}
+    }
+}
