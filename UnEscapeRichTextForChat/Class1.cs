09363fe870b0f103acf60e9329713484e1db84a1
DestroyedClone <destroyedclone@gmail.com>
2021-09-29 01:39:51 -0700

    Made unescaperichtext mod

---
+++ UnEscapeRichTextForChat/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using RoR2;
+using BepInEx;
+
+namespace UnEscapeRichTextForChat
+{
+    [BepInPlugin("com.DestroyedClone.RichTextForChat", "Rich Text For Chat", "1.0.0")]
+    public class Class1 : BaseUnityPlugin
+    {
+        public void Awake()
+        {
+            On.RoR2.Util.EscapeRichTextForTextMeshPro += (On.RoR2.Util.orig_EscapeRichTextForTextMeshPro orig, string rtString) =>
+            {
+                return rtString;
+            };
+        }
+    }
+}
