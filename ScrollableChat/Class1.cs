d7ac1b41e1393b3e0a44d836584170a1cf2ff368
DestroyedClone <destroyedclone@gmail.com>
2021-10-17 01:36:59 -0700

    config and code adjustments

--- ScrollableChat/Class1.cs
+++ ScrollableChat/Class1.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 using R2API;
 using R2API.Utils;
 using BepInEx.Configuration;
+using UnityEngine.UI;
 
 [module: UnverifiableCode]
 #pragma warning disable CS0618 // Type or member is obsolete
@@ -23,20 +24,30 @@ namespace ChatboxEdit
 
         public static ConfigEntry<bool> ScrollableChat;
         public static ConfigEntry<float> ScrollableChatSensitivity;
-        public static ConfigEntry<bool> NoMaskChat;
         public static ConfigEntry<float> FadeDelay;
         public static ConfigEntry<float> FadeDuration;
+
+        public static ConfigEntry<ChatExpandMode> cfgChatExpandeMode;
+
+        public enum ChatExpandMode
+        {
+            Disabled,
+            Expanded,
+            Unlimited
+        }
 
         public void Start()
         {
             ScrollableChat = Config.Bind("Scrollable Chat","Enable",true,"If true, allows you to scroll the chat by scrolling or clicking and dragging." +
                 "\nThe very top of the chat (about 5 messages) will scroll in the opposite direction regardless of choice, weird vanilla thing.");
             ScrollableChatSensitivity = Config.Bind("Scrollable Chat", "Scroll Sensitivity", 5f, "If Scrollable Chat is active, then adjusts the sensitivity of scrolling.");
-            NoMaskChat = Config.Bind("", "No Chat Mask", true, "If true, the mask that causes the chat to only reveal the last few messages gets removed, showing all messages." +
-                "\nMessages may go off screen, and/or cover the screen, so I'd recommend not setting a high chat fade delay.");
             FadeDelay = Config.Bind("", "Chat Fade Delay", 5f, "");
             FadeDuration = Config.Bind("", "Chat Fade Duration", 5f, "");
             RoR2.Chat.cvChatMaxMessages.value = Config.Bind("", "Max Chat Messages", 30, "").Value;
+
+            cfgChatExpandeMode = Config.Bind("", "Chat Expand Mode", ChatExpandMode.Disabled, "0. Disabled." +
+                "\nExpanded = While the input is open, your visible chat increases by about 5 lines." +
+                "\nUnlimited = All messages can show up on screen, can cover your screen.");
 
             var chatbox = Prefab.GetComponent<RoR2.UI.ChatBox>();
             var chatboxLobby = PrefabLobby.GetComponent<RoR2.UI.ChatBox>();
@@ -47,12 +58,22 @@ namespace ChatboxEdit
             chatboxLobby.fadeTimer = FadeDelay.Value;
             chatboxLobby.fadeDuration = FadeDuration.Value;
 
-
-            var scrollRect = chatbox.scrollRect;
-            var scrollRectLobby = chatboxLobby.scrollRect;
+            switch (cfgChatExpandeMode.Value)
+            {
+                case ChatExpandMode.Expanded:
+                    chatbox.allowExpandedChatbox = true;
+                    break;
+                case ChatExpandMode.Unlimited:
+                    Prefab.transform.Find("StandardRect/Scroll View/Viewport/MessageArea/Text Area").GetComponent<RectMask2D>().enabled = false;
+                    Prefab.transform.Find("StandardRect/Scroll View/Viewport").GetComponent<Mask>().enabled = false;
+                    Prefab.transform.Find("StandardRect/Scroll View/Viewport").GetComponent<Image>().enabled = false;
+                    break;
+            }
 
             if (ScrollableChat.Value)
             {
+                var scrollRect = chatbox.scrollRect;
+                var scrollRectLobby = chatboxLobby.scrollRect;
                 scrollRect.enabled = true;
                 scrollRect.scrollSensitivity = ScrollableChatSensitivity.Value;
                 scrollRect.vertical = true;
@@ -61,7 +82,6 @@ namespace ChatboxEdit
                 scrollRectLobby.scrollSensitivity = ScrollableChatSensitivity.Value;
                 scrollRectLobby.vertical = true;
             }
-
             
             /*scrollRect.transform.Find("Viewport/MessageArea/Text Area/MessageArea Input Carat").GetComponent<TMPro.TMP_SelectionCaret>().color = new Color32()
             {
