7a2d7a90f802178bcfe9822ad234921ec0ede181
DestroyedClone <destroyedclone@gmail.com>
2021-10-19 11:28:24 -0700

    adjustment

--- ShowEquipment/Class1.cs
+++ ShowEquipment/Class1.cs
@@ -6,12 +6,13 @@ using System.Security.Permissions;
 using UnityEngine;
 using UnityEngine.UI;
 using RoR2.UI;
+using BepInEx.Configuration;
 
 [module: UnverifiableCode]
 #pragma warning disable CS0618 // Type or member is obsolete
 [assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
 #pragma warning restore CS0618 // Type or member is obsolete
-[assembly: HG.Reflection.SearchableAttribute.OptIn]
+
 namespace ShowEquipment
 {
     [BepInPlugin("com.DestroyedClone.ShowEquipment", "Show Equipment", "1.0.0")]
@@ -19,34 +20,120 @@ namespace ShowEquipment
     [NetworkCompatibility(CompatibilityLevel.NoNeedForSync, VersionStrictness.DifferentModVersionsAreOk)]
     public class ShowEquipmentPlugin : BaseUnityPlugin
     {
-        public static BodyIndex EquipmentDroneBodyIndex;
+        public static ConfigEntry<NameEquipmentMode> AllyCard_ShowEquipment;
+
+        public static ConfigEntry<NameEquipmentCategory> cfgNameEquipmentCategory;
+        public static ConfigEntry<NameEquipmentMode> cfgNameEquipmentMode;
+
+        public static MasterCatalog.MasterIndex EquipmentDroneMasterIndex;
         public static GameObject AllyCard = Resources.Load<GameObject>("prefabs/ui/AllyCard");
+
+
+        public enum NameEquipmentMode
+        {
+            Disabled,
+            EquipmentDrones,
+            Any
+        }
+
+        public enum NameEquipmentCategory
+        {
+            AllyCard,
+            Any
+        }
 
         public void Start()
         {
-            On.RoR2.Util.GetBestBodyName += Util_GetBestBodyName;
-            On.RoR2.UI.AllyCardController.Awake += AllyCardController_Awake;
-            //SetupShit();
+            AllyCard_ShowEquipment = Config.Bind("Ally Cards", "Show Equipment Icon", NameEquipmentMode.Any);
+            cfgNameEquipmentCategory = Config.Bind("", "Name Category", NameEquipmentCategory.AllyCard, 
+                "AllyCard = Shows up only on the ally cards" +
+                "\nAny = Shows up on most, if not all, occurences.");
+            cfgNameEquipmentMode = Config.Bind("General", "Name Mode", NameEquipmentMode.EquipmentDrones,
+                "Disabled - Self Explanatory." +
+                "\nEquipmentDrones = Only equipment drones will have their name changed." +
+                "\nAny = Most bodies will have their name changed.");
+            On.RoR2.MasterCatalog.Init += MasterCatalog_Init;
+
+            if (AllyCard_ShowEquipment.Value > NameEquipmentMode.Disabled)
+            {
+                On.RoR2.UI.AllyCardController.Awake += AllyCardController_Awake;
+            }
+
+            if (cfgNameEquipmentMode.Value > NameEquipmentMode.Disabled)
+            {
+                switch (cfgNameEquipmentCategory.Value)
+                {
+                    case NameEquipmentCategory.AllyCard:
+                        On.RoR2.UI.AllyCardController.UpdateInfo += AllyCardController_UpdateInfo;
+                        break;
+                    default:
+                        On.RoR2.Util.GetBestBodyName += Util_GetBestBodyName;
+                        On.RoR2.Util.GetBestMasterName += Util_GetBestMasterName;
+                        break;
+                }
+            }
         }
+
+        private string Util_GetBestMasterName(On.RoR2.Util.orig_GetBestMasterName orig, CharacterMaster characterMaster)
+        {
+            var origName = orig(characterMaster);
+
+            if (characterMaster && characterMaster.inventory && characterMaster.inventory.currentEquipmentIndex != EquipmentIndex.None)
+            {
+                if (characterMaster.teamIndex == LocalUserManager.GetFirstLocalUser().cachedMaster.teamIndex)
+                {
+                    if ((characterMaster.masterIndex == EquipmentDroneMasterIndex && cfgNameEquipmentMode.Value == NameEquipmentMode.EquipmentDrones)
+                        || cfgNameEquipmentMode.Value == NameEquipmentMode.Any)
+                    {
+                        origName += $" ({Language.GetString(EquipmentCatalog.GetEquipmentDef(characterMaster.inventory.currentEquipmentIndex).nameToken)})";
+                    }
+                    //35 char limit ish before truncation
+                }
+            }
+            return origName;
+
+        }
+
+        private void MasterCatalog_Init(On.RoR2.MasterCatalog.orig_Init orig)
+        {
+            orig();
+            EquipmentDroneMasterIndex = MasterCatalog.FindMasterIndex("EquipmentDroneMaster");
+            //Debug.Log($"Equipment Drone MAster Index: {EquipmentDroneMasterIndex.i}");
+            On.RoR2.MasterCatalog.Init -= MasterCatalog_Init;
+        }
+
+        private void AllyCardController_UpdateInfo(On.RoR2.UI.AllyCardController.orig_UpdateInfo orig, AllyCardController self)
+        {
+            orig(self);
+            CharacterMaster master = self.sourceMaster;
+            if (master
+                && (( master.masterIndex == EquipmentDroneMasterIndex && cfgNameEquipmentMode.Value == NameEquipmentMode.EquipmentDrones) || cfgNameEquipmentMode.Value == NameEquipmentMode.Any)
+                && master.inventory && master.inventory.currentEquipmentIndex != EquipmentIndex.None)
+            {
+                if (master.teamIndex == LocalUserManager.GetFirstLocalUser()?.cachedMaster?.teamIndex)
+                {
+                    self.nameLabel.text += $" ({Language.GetString(EquipmentCatalog.GetEquipmentDef(master.inventory.currentEquipmentIndex).nameToken)})";
+                }
+            }
+        }
+
 
         private void AllyCardController_Awake(On.RoR2.UI.AllyCardController.orig_Awake orig, AllyCardController self)
         {
             orig(self);
-            var portrait = self.portraitIconImage.gameObject;
-            var display = Instantiate(portrait.transform.Find("CriticallyHurt"), portrait.transform);
-            display.name = "EquipmentDisplay";
-            display.transform.localPosition = new Vector3(-25, 0, 0);
-            var component = display.gameObject.AddComponent<AllyCardEquipment>();
-            component.image = display.GetComponent<Image>();
-            component.allyCardController = self;
+            if ((AllyCard_ShowEquipment.Value == NameEquipmentMode.EquipmentDrones && self.sourceMaster && self.sourceMaster.masterIndex == EquipmentDroneMasterIndex)
+                || (AllyCard_ShowEquipment.Value == NameEquipmentMode.Any))
+            {
+                var portrait = self.portraitIconImage.gameObject;
+                var display = Instantiate(portrait.transform.Find("CriticallyHurt"), portrait.transform);
+                display.name = "EquipmentDisplay";
+                display.transform.localPosition = new Vector3(-25, 0, 0);
+                var component = display.gameObject.AddComponent<AllyCardEquipment>();
+                component.image = display.GetComponent<Image>();
+                component.allyCardController = self;
+            }
         }
 
-        [RoR2.SystemInitializer(dependencies: typeof(RoR2.BodyCatalog))]
-        public static void GetBodyIndex()
-        {
-            EquipmentDroneBodyIndex = BodyCatalog.FindBodyIndex("EQUIPMENTDRONE");
-            Debug.Log(EquipmentDroneBodyIndex);
-        }
 
         public static void SetupShit()
         {
@@ -71,7 +158,11 @@ namespace ShowEquipment
             {
                 if (characterBody.teamComponent.teamIndex == LocalUserManager.GetFirstLocalUser().cachedMaster.teamIndex)
                 {
-                    origName += $" ({Language.GetString(EquipmentCatalog.GetEquipmentDef(characterBody.inventory.currentEquipmentIndex).nameToken)})";
+                    if ( (characterBody.master && characterBody.master.masterIndex == EquipmentDroneMasterIndex && cfgNameEquipmentMode.Value == NameEquipmentMode.EquipmentDrones)
+                        || cfgNameEquipmentMode.Value == NameEquipmentMode.Any)
+                    {
+                        origName += $" ({Language.GetString(EquipmentCatalog.GetEquipmentDef(characterBody.inventory.currentEquipmentIndex).nameToken)})";
+                    }
                     //35 char limit ish before truncation
                 }
             }
@@ -91,7 +182,6 @@ namespace ShowEquipment
                 {
                     inventory.onInventoryChanged += Inventory_onInventoryChanged;
                     UpdateSprite();
-                    image.enabled = true;
                 }
             }
 
@@ -115,7 +205,7 @@ namespace ShowEquipment
                     image.enabled = true;
                 } else
                 {
-                    image.sprite = null;
+                    image.enabled = false;
                 }
             }
         }
