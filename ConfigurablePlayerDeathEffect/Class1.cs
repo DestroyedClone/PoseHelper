9bd70aae123920729ae4ddb9f9a46f2cb0e4b6f8
DestroyedClone <destroyedclone@gmail.com>
2021-10-28 23:53:48 -0700

    neat

---
+++ ConfigurablePlayerDeathEffect/Class1.cs
@@ -0,0 +1,25 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using UnityEngine;
+
+namespace ConfigurablePlayerDeathEffect
+{
+    [BepInPlugin("com.DestroyedClone.CFGPlayerDeathEffect", "Configurable Player Death Effect", "1.0.0")]
+    [BepInDependency(R2API.R2API.PluginGUID, R2API.R2API.PluginVersion)]
+    public class Class1 : BaseUnityPlugin
+    {
+        public static GameObject effect = Resources.Load<GameObject>("prefabs/temporaryvisualeffects/PlayerDeathEffect");
+
+        public static ConfigEntry<bool> cfgEnableDeathEffect;
+
+        public void Start()
+        {
+            cfgEnableDeathEffect = Config.Bind<bool>("", "Enable Death Effect", false, "If true, disables the red post processing from appearing.");
+            if (!cfgEnableDeathEffect.Value)
+            {
+                effect.transform.Find("CameraEffect/PP").gameObject.SetActive(false);
+                //effect.transform.Find("CameraEffect/PP").GetComponent<PostProcessDuration>().maxDuration = 0f;
+            }
+        }
+    }
+}
---
+++ ConfigurablePlayerDeathEffect/README.md
@@ -0,0 +1,5 @@
+﻿# Configurable Player Death Effect
+
+Currently just adds an option to toggle the red post processing from showing up, which is set to false (as in shouldn't show up) by default.
+
+Useful for splitscreen multiplayer when you don't want to blind your teammate.
