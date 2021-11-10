71fb3e5fd09fb48c7b1943988d5b6257a7f889af
DestroyedClone <destroyedclone@gmail.com>
2021-11-10 12:20:34 -0800

    mod

---
+++ NoHereticTransform/Class1.cs
@@ -0,0 +1,23 @@
+﻿using BepInEx;
+using R2API.Utils;
+using RoR2;
+
+namespace NoHereticTransform
+{
+    [BepInPlugin("com.DestroyedClone.NoHereticTransform", "No Heretic Transform", "1.0.0")]
+    [BepInDependency(R2API.R2API.PluginGUID, R2API.R2API.PluginVersion)]
+    [NetworkCompatibility(CompatibilityLevel.NoNeedForSync, VersionStrictness.DifferentModVersionsAreOk)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public void Awake()
+        {
+            On.RoR2.CharacterMaster.TransformBody += CharacterMaster_TransformBody;
+        }
+
+        private void CharacterMaster_TransformBody(On.RoR2.CharacterMaster.orig_TransformBody orig, CharacterMaster self, string bodyName)
+        {
+            if (bodyName == "HereticBody")
+                return;
+        }
+    }
+}
