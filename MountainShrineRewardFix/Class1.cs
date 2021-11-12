324ba7ae48f7ce8811ff8dae6a03327c2e070870
DestroyedClone <destroyedclone@gmail.com>
2021-11-12 15:18:16 -0800

    mod

---
+++ MountainShrineRewardFix/Class1.cs
@@ -0,0 +1,119 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using Mono.Cecil.Cil;
+using MonoMod.Cil;
+using R2API.Utils;
+using RoR2;
+using System;
+using System.Collections.Generic;
+using System.Security;
+using System.Security.Permissions;
+using UnityEngine;
+
+[module: UnverifiableCode]
+#pragma warning disable CS0618 // Type or member is obsolete
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+#pragma warning restore CS0618 // Type or member is obsolete
+
+namespace BossDropRewardDelay
+{
+    [BepInPlugin("com.DestroyedClone.BossDropRewardDelay", "Boss Drop Reward Delay", "1.0.0")]
+    [BepInDependency(R2API.R2API.PluginGUID, R2API.R2API.PluginVersion)]
+    [NetworkCompatibility(CompatibilityLevel.NoNeedForSync, VersionStrictness.DifferentModVersionsAreOk)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public static ConfigEntry<float> cfgSpawnDelay;
+
+        public void Awake()
+        {
+            cfgSpawnDelay = Config.Bind("", "Delay Between Drops", 0.3f, "The amount of time, in seconds, between each drop.");
+
+            IL.RoR2.BossGroup.DropRewards += BossGroup_DropRewards;
+        }
+
+        private void BossGroup_DropRewards(MonoMod.Cil.ILContext il)
+        {
+            ILCursor c = new ILCursor(il);
+            c.GotoNext(
+                 x => x.MatchStloc(5),
+                 x => x.MatchLdcI4(0),
+                x => x.MatchStloc(6)
+            );
+            c.Index += 2;
+            c.Emit(OpCodes.Ldarg_0);    //self
+            c.Emit(OpCodes.Ldloc_2);    //PickupIndex
+            c.Emit(OpCodes.Ldloc, 4);    //vector
+            c.Emit(OpCodes.Ldloc, 5);    //rotation
+            c.Emit(OpCodes.Ldloc, 3);    //scaledRewardCount
+            c.EmitDelegate<Func<int, BossGroup, PickupIndex, Vector3, Quaternion, int, int>>((val, self, pickupIndex, vector, rotation, scaledRewardCount) =>
+            {
+                if (self && !self.GetComponent<DelayedBossRewards>())
+                {
+                    var component = self.gameObject.AddComponent<DelayedBossRewards>();
+                    component.rng = self.rng;
+                    component.num = scaledRewardCount;
+                    component.pickupIndex = pickupIndex;
+                    component.bossDrops = self.bossDrops;
+                    component.bossDropChance = self.bossDropChance;
+                    component.dropPosition = self.dropPosition;
+                    component.vector = vector;
+                    component.rotation = rotation;
+                }
+                return int.MaxValue;    //Prevent vanilla code from being run
+            });
+        }
+
+        public class DelayedBossRewards : MonoBehaviour
+        {
+            // Carry-overs
+            public Xoroshiro128Plus rng;
+
+            public List<PickupIndex> bossDrops;
+            public float bossDropChance;
+            public Transform dropPosition;
+
+            private int i = 0;
+            public PickupIndex pickupIndex;
+            public int num = 1;
+            public Quaternion rotation;
+            public Vector3 vector;
+
+            public List<PickupIndex> rewards = new List<PickupIndex>();
+
+            public float age = 0;
+            public float delay = 0.3f;
+
+            public void Awake()
+            {
+                delay = cfgSpawnDelay.Value;
+            }
+
+            public void FixedUpdate()
+            {
+                // Stopwatch Check
+                age += Time.fixedDeltaTime;
+                if (age < delay)
+                {
+                    return;
+                }
+                // Drop Count Check
+                if (i < num)
+                {
+                    PickupIndex pickupIndex2 = pickupIndex;
+                    if (bossDrops.Count > 0 && rng.nextNormalizedFloat <= bossDropChance)
+                    {
+                        pickupIndex2 = rng.NextElementUniform<PickupIndex>(bossDrops);
+                    }
+                    PickupDropletController.CreatePickupDroplet(pickupIndex2, dropPosition.position, vector);
+                    i++;
+                    vector = rotation * vector;
+                    age = 0;
+                }
+                else
+                {
+                    enabled = false;
+                }
+            }
+        }
+    }
+}
---
+++ MountainShrineRewardFix/README.md
@@ -0,0 +1,12 @@
+﻿# Boss Drop Reward Delay
+
+This mod adds a short delay between rewards from the teleporter drops. Since modding can increase the amount of drops to high values (such as 16 playercounts x 4 mountain shrines), the amount of drops that are produced becomes too much for the system to handle, either resulting in a long-lasting lagspike or a crash.
+
+The configuration defaults to 0.3s, but the values can be adjusted by the user.
+
+![enter image description here](https://raw.githubusercontent.com/DestroyedClone/PoseHelper/master/MountainShrineRewardFix/preview.gif)
+
+## Credits
+Moffein - IL code, consultation
+
+> Written with [StackEdit](https://stackedit.io/).
