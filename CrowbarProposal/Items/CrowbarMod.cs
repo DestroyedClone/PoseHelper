fb76dbb2fd9458e5e4ac3eaacd6cce25a490e123
DestroyedClone <destroyedclone@gmail.com>
2021-11-12 20:38:42 -0800

    anon sama

---
+++ CrowbarProposal/Items/CrowbarMod.cs
@@ -0,0 +1,127 @@
+﻿using BepInEx.Configuration;
+using R2API;
+using RoR2;
+using UnityEngine;
+using static CrowbarProposal.Main;
+using System.Collections.Generic;
+
+namespace CrowbarProposal.Items
+{
+    public class CrowbarMod : ItemBase<CrowbarMod>
+    {
+        public override string ItemName => "Fragile Crowbar";
+
+        public override string ItemLangTokenName => "FRAGILE_CROWBAR";
+
+        public override string ItemPickupDesc => "The first damage you deal to an enemy is increased.";
+
+        public override string ItemFullDescription => "The first hit you've done against an enemy deals <style=cIsDamage>+35%</style> <style=cStack>(+35% per stack)</style> damage. This damage is DOUBLED to enemies above <style=cIsDamage>90% health</style>.";
+
+        public override string ItemLore => "";
+
+        public override ItemTier Tier => ItemTier.Tier1;
+
+        public override GameObject ItemModel => RoR2Content.Items.Crowbar.pickupModelPrefab;
+
+        public override Sprite ItemIcon => RoR2Content.Items.Crowbar.pickupIconSprite;
+
+        public static float damagePerStack = 0.35f;
+
+        public override void Init(ConfigFile config)
+        {
+            CreateConfig(config);
+            CreateLang();
+            CreateItem();
+            Hooks();
+        }
+
+        public override void CreateConfig(ConfigFile config)
+        {
+
+        }
+
+        public override ItemDisplayRuleDict CreateItemDisplayRules()
+        {
+            return new ItemDisplayRuleDict();
+        }
+
+        public override void Hooks()
+        {
+            // Damage Doubler Based on Healthy
+            //On.RoR2.HealthComponent.TakeDamage += HealthComponent_TakeDamage;
+            // Original suggested
+            On.RoR2.HealthComponent.TakeDamage += HealthComponent_TakeDamage1;
+        }
+
+        private void HealthComponent_TakeDamage1(On.RoR2.HealthComponent.orig_TakeDamage orig, HealthComponent self, DamageInfo damageInfo)
+        {
+            var crowbarTracker = self.GetComponent<FragileCrowbarTracker>();
+            if (!crowbarTracker || !crowbarTracker.attackerObjects.Contains(damageInfo.attacker))
+            {
+                var attackerBody = damageInfo.attacker.GetComponent<CharacterBody>();
+                if (attackerBody)
+                {
+                    var master = attackerBody.master;
+                    if (master)
+                    {
+                        var stacks = GetCount(master);
+                        if (stacks > 0)
+                        {
+                            damageInfo.damage *= 1f + 0.75f * (float)stacks;
+                            EffectManager.SimpleImpactEffect(HealthComponent.AssetReferences.crowbarImpactEffectPrefab, damageInfo.position, -damageInfo.force, true);
+
+                            if (!crowbarTracker)
+                            {
+                                crowbarTracker = self.gameObject.AddComponent<FragileCrowbarTracker>();
+                            }
+                            crowbarTracker.attackerObjects.Add(damageInfo.attacker);
+                        }
+                    }
+                }
+            }
+
+            orig(self, damageInfo);
+        }
+
+        private void HealthComponent_TakeDamage(On.RoR2.HealthComponent.orig_TakeDamage orig, HealthComponent self, DamageInfo damageInfo)
+        {
+            var crowbarTracker = self.GetComponent<FragileCrowbarTracker>();
+            if (!crowbarTracker || crowbarTracker.attackerObjects.Contains(damageInfo.attacker))
+            {
+                var attackerBody = damageInfo.attacker.GetComponent<CharacterBody>();
+                if (attackerBody)
+                {
+                    var master = attackerBody.master;
+                    if (master)
+                    {
+                        var stacks = GetCount(master);
+                        if (stacks > 0)
+                        {
+                            var damageModifier = 1f + stacks * damagePerStack;
+
+                            if (self.combinedHealth >= self.fullCombinedHealth * 0.9f)
+                            {
+                                damageModifier *= 2f;
+                                EffectManager.SimpleImpactEffect(HealthComponent.AssetReferences.crowbarImpactEffectPrefab, damageInfo.position, -damageInfo.force, true);
+                            }
+                            damageInfo.damage *= damageModifier;
+                            if (!crowbarTracker)
+                            {
+                                crowbarTracker = self.gameObject.AddComponent<FragileCrowbarTracker>();
+                            }
+                            crowbarTracker.attackerObjects.Add(damageInfo.attacker);
+                        }
+                    }
+                }
+            }
+
+            orig(self, damageInfo);
+        }
+
+        public class FragileCrowbarTracker : MonoBehaviour
+        {
+            // Keeps track of all masters that have damaged this being.
+            public List<GameObject> attackerObjects = new List<GameObject>();
+        }
+    }
+}
---
+++ CrowbarProposal/Main.cs
@@ -0,0 +1,72 @@
+﻿using BepInEx;
+using CrowbarProposal.Items;
+using R2API;
+using R2API.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+
+namespace CrowbarProposal
+{
+    [BepInPlugin(ModGuid, ModName, ModVer)]
+    [BepInDependency(R2API.R2API.PluginGUID, R2API.R2API.PluginVersion)]
+    [NetworkCompatibility(CompatibilityLevel.EveryoneMustHaveMod, VersionStrictness.EveryoneNeedSameModVersion)]
+    [R2APISubmoduleDependency(nameof(ItemAPI), nameof(LanguageAPI), nameof(ArtifactAPI), nameof(EliteAPI))]
+    public class Main : BaseUnityPlugin
+    {
+        public const string ModGuid = "com.MyUsername.MyModName";
+        public const string ModName = "My Mod's Title and if we see this exact name on Thunderstore we will deprecate your mod";
+        public const string ModVer = "0.0.1";
+
+        //public static AssetBundle MainAssets;
+
+        public List<ItemBase> Items = new List<ItemBase>();
+
+        private void Awake()
+        {
+            // Don't know how to create/use an asset bundle, or don't have a unity project set up?
+            // Look here for info on how to set these up: https://github.com/KomradeSpectre/AetheriumMod/blob/rewrite-master/Tutorials/Item%20Mod%20Creation.md#unity-project
+            /*
+            using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("CrowbarProposal.my_assetbundlefile"))
+            {
+                MainAssets = AssetBundle.LoadFromStream(stream);
+            }
+            */
+
+            //This section automatically scans the project for all items
+            var ItemTypes = Assembly.GetExecutingAssembly().GetTypes().Where(type => !type.IsAbstract && type.IsSubclassOf(typeof(ItemBase)));
+
+            foreach (var itemType in ItemTypes)
+            {
+                ItemBase item = (ItemBase)System.Activator.CreateInstance(itemType);
+                if (ValidateItem(item, Items))
+                {
+                    item.Init(Config);
+                }
+            }
+        }
+
+        /// <summary>
+        /// A helper to easily set up and initialize an item from your item classes if the user has it enabled in their configuration files.
+        /// <para>Additionally, it generates a configuration for each item to allow blacklisting it from AI.</para>
+        /// </summary>
+        /// <param name="item">A new instance of an ItemBase class."</param>
+        /// <param name="itemList">The list you would like to add this to if it passes the config check.</param>
+        public bool ValidateItem(ItemBase item, List<ItemBase> itemList)
+        {
+            var enabled = Config.Bind<bool>("Item: " + item.ItemName, "Enable Item?", true, "Should this item appear in runs?").Value;
+            var aiBlacklist = Config.Bind<bool>("Item: " + item.ItemName, "Blacklist Item from AI Use?", false, "Should the AI not be able to obtain this item?").Value;
+            if (enabled)
+            {
+                itemList.Add(item);
+                if (aiBlacklist)
+                {
+                    item.AIBlacklisted = true;
+                }
+            }
+            return enabled;
+        }
+    }
+}
