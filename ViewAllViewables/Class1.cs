b10913830cc770a93766514d00437685c1e07422
DestroyedClone <destroyedclone@gmail.com>
2021-11-13 10:33:31 -0800

    Performance Improvement

--- ViewAllViewables/Class1.cs
+++ ViewAllViewables/Class1.cs
@@ -1,10 +1,11 @@
 ﻿using BepInEx;
+using BepInEx.Configuration;
+using R2API.Utils;
 using RoR2;
-using UnityEngine;
 using System.Linq;
 using System.Security;
 using System.Security.Permissions;
-using R2API.Utils;
+using UnityEngine;
 
 [module: UnverifiableCode]
 #pragma warning disable CS0618 // Type or member is obsolete
@@ -13,40 +14,54 @@ using R2API.Utils;
 
 namespace ViewAllViewables
 {
-    [BepInPlugin("com.DestroyedClone.ViewAllViewables", "View All Viewables", "1.0.0")]
+    [BepInPlugin("com.DestroyedClone.ViewAllViewables", "View All Viewables", "1.1.0")]
     [BepInDependency(R2API.R2API.PluginGUID, R2API.R2API.PluginVersion)]
     [NetworkCompatibility(CompatibilityLevel.NoNeedForSync, VersionStrictness.DifferentModVersionsAreOk)]
     public class Plugin : BaseUnityPlugin
     {
+        public static ConfigEntry<bool> cfgPrintProgressToConsole;
+        public static ConfigEntry<bool> cfgViewForAllLocalUsers;
+
         public void Awake()
         {
+            cfgPrintProgressToConsole = Config.Bind("", "Print Viewed Content", false, "If true, then the game will print the names of the viewed content into console.");
+            cfgViewForAllLocalUsers = Config.Bind("", "View For All Local Users", true, "If true, then the game will apply the viewed setting to all local users. If false, only the first user will be affected.");
+
             R2API.Utils.CommandHelper.AddToConsoleWhenReady();
         }
 
         [ConCommand(commandName = "viewall", flags = ConVarFlags.ExecuteOnServer, helpText = "Marks all unviewed icons as viewed. May cause the game to freeze momentarily. Use in lobby.")]
         [System.Diagnostics.CodeAnalysis.SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "Console Command")]
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "Empty Arg required")]
         private static void ViewAllUnviewed(ConCommandArgs args)
         {
             UserProfile userProfile = args.GetSenderLocalUser().userProfile;
             var viewableNames = (from node in ViewablesCatalog.rootNode.Descendants()
                                  where node.shouldShowUnviewed(userProfile)
                                  select node.fullName);
-            var hasScanned = false;
+            var amountScanned = 0;
             foreach (var viewableName in viewableNames.ToList())
             {
-                Debug.Log(viewableName);
                 if (string.IsNullOrEmpty(viewableName))
                 {
                     continue;
                 }
-                foreach (LocalUser localUser in LocalUserManager.readOnlyLocalUsersList)
+                amountScanned++;
+                if (cfgPrintProgressToConsole.Value)
+                    Debug.Log(viewableName);
+                if (cfgViewForAllLocalUsers.Value)
                 {
-                    localUser.userProfile.MarkViewableAsViewed(viewableName);
-                    hasScanned = true;
+                    foreach (LocalUser localUser in LocalUserManager.readOnlyLocalUsersList)
+                    {
+                        localUser.userProfile.MarkViewableAsViewed(viewableName);
+                    }
+                }
+                else
+                {
+                    LocalUserManager.readOnlyLocalUsersList[0].userProfile.MarkViewableAsViewed(viewableName);
                 }
             }
-            if (!hasScanned) Debug.Log("Nothing left to view!");
+            if (amountScanned != 0) Debug.Log($"Viewed {amountScanned} unviewed content!");
+            else Debug.Log("Nothing left to view!");
         }
     }
 }
--- ViewAllViewables/README.md
+++ ViewAllViewables/README.md
@@ -1,18 +1,24 @@
 # View All Viewables
-Stuff you haven't viewed yet have a yellow highlight over them, so this mod adds a command that views all unviewed viewables.
+
+| [![github issues/request link](https://raw.githubusercontent.com/DestroyedClone/PoseHelper/master/PoseHelper/github_link.webp)](https://github.com/DestroyedClone/PoseHelper/issues) | [![discord invite](https://raw.githubusercontent.com/DestroyedClone/PoseHelper/master/PoseHelper/discord_link.webp)](discord.gg/DpHu3qXMHK) |
+|--|--|
+
+While playing the game, things you haven't viewed yet have a yellow highlight over them that requires you to mouse over them to "view" them, removing the highlight. This mod adds a command that "views" all unviewed things. Do not confused this mod with unlocking all locked things.
 
 Usage: 
 
  1. Start a singleplayer lobby
  2. Open Console (Ctrl + Alt + `)
  3. "viewall", enter.
- 4. Freeze for ~10 seconds
 
-This command may or may not be destructive, so I'd recommend backing up your profile. This mod is mainly for those with profiles that reset alot and/or use UnlockAll alot.
-
-The freeze time was about 10 seconds on a fresh vanilla profile, so your mileage will vary.
+This mod is mainly for those with profiles that reset alot and/or use UnlockAll alot.
 
 # Changelog
+v1.1.0 - Performance Adjustment
+- Config: (Disabled by default) Prints the name of the viewed viewable to console.
+	- The cause of the freezing was actually printing the name to console for every unviewed pretty much all at once. For performance, it is disabled.
+- Config: (Enabled) View For All Local Users: If there are more than one local user active (splitscreen?) then it will view that content for all users. Otherwise, it will only view it for the first user.
+
 v1.0.0 - Release
 
 > Written with [StackEdit](https://stackedit.io/).
