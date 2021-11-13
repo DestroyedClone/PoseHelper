4b1c4b82727625e65ef7e96f966b74f1d9979156
DestroyedClone <destroyedclone@gmail.com>
2021-11-13 10:46:56 -0800

    readme update

--- NoHereticTransform/Class1.cs
+++ NoHereticTransform/Class1.cs
@@ -1,23 +1,34 @@
 ﻿using BepInEx;
+using BepInEx.Configuration;
 using R2API.Utils;
 using RoR2;
 
 namespace NoHereticTransform
 {
-    [BepInPlugin("com.DestroyedClone.NoHereticTransform", "No Heretic Transform", "1.0.0")]
+    [BepInPlugin("com.DestroyedClone.NoHereticTransform", "No Heretic Transform", "1.0.1")]
     [BepInDependency(R2API.R2API.PluginGUID, R2API.R2API.PluginVersion)]
     [NetworkCompatibility(CompatibilityLevel.NoNeedForSync, VersionStrictness.DifferentModVersionsAreOk)]
     public class Plugin : BaseUnityPlugin
     {
+        public static ConfigEntry<bool> cfgRequireBeads;
+
         public void Awake()
         {
+            cfgRequireBeads = Config.Bind("", "Require Beads to Transform", true, "If true, then you will transform into the Heretic only if you have the full Heresy set and a Beads of Fealty.");
+
             On.RoR2.CharacterMaster.TransformBody += CharacterMaster_TransformBody;
         }
 
         private void CharacterMaster_TransformBody(On.RoR2.CharacterMaster.orig_TransformBody orig, CharacterMaster self, string bodyName)
         {
             if (bodyName == "HereticBody")
+            {
+                if (self.inventory && self.inventory.GetItemCount(RoR2Content.Items.LunarTrinket) > 0)
+                {
+                    orig(self, bodyName);
+                }
                 return;
+            }
         }
     }
 }
--- NoHereticTransform/README.md
+++ NoHereticTransform/README.md
@@ -5,3 +5,11 @@ Prevents turning into the [Heretic](https://riskofrain2.fandom.com/wiki/Heretic)
 Should only require the host to need it, since the method only runs on the network.
 
 ![preview](https://raw.githubusercontent.com/DestroyedClone/PoseHelper/master/NoHereticTransform/noheresypreview.gif)
+
+With the configuration setting "Require Beads to Transform" (enabled by default), picking up a [Beads of Fealty](https://riskofrain2.fandom.com/wiki/Beads_of_Fealty) will allow you to transform
+
+![preview for config setting](https://raw.githubusercontent.com/DestroyedClone/PoseHelper/master/NoHereticTransform/noheresybeadspreview.gif)
+
+## Changelog
+v1.0.1 - Added option to transform if you have the beads of Fealty (suggested by gaforb)
+v1.0.0 - Release
