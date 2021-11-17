013fa3f64f9d0fe0c45011e6ad46b21c81376fd1
DestroyedClone <destroyedclone@gmail.com>
2021-11-16 22:49:49 -0800

    dumb shit

---
+++ UnknownErrorFixes/Class1.cs
@@ -0,0 +1,27 @@
+﻿using BepInEx;
+using System.Security.Permissions;
+
+#pragma warning disable CS0618 // Type or member is obsolete
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+#pragma warning restore CS0618 // Type or member is obsolete
+
+namespace UnknownErrorFixes
+{
+    [BepInPlugin("com.DestroyedClone.UnknownErrorFixes", "UnknownErrorFixes", "1.0.0")]
+    public class Main : BaseUnityPlugin
+    {
+        public void Start()
+        {
+            On.RoR2.Projectile.ProjectileSteerTowardTarget.FixedUpdate += ProjectileSteerTowardTarget_FixedUpdate;
+        }
+
+        private void ProjectileSteerTowardTarget_FixedUpdate(On.RoR2.Projectile.ProjectileSteerTowardTarget.orig_FixedUpdate orig, RoR2.Projectile.ProjectileSteerTowardTarget self)
+        {
+            if (self.targetComponent)
+            {
+                orig(self);
+            }
+            return;
+        }
+    }
+}
---
+++ UnknownErrorFixes/README.md
@@ -0,0 +1,12 @@
+﻿# Unknown Error Fixes
+
+If you've never gotten these errors then there's no point in downloading this mod.
+
+A bunch of errors I have no idea how it could be fixed. Feel free to send me more shit to add nullchecks to.
+
+## ProjectileSteerTowardsTarget
+> [Error  : Unity Log] NullReferenceException: Object reference not set to an instance of an object
+Stack trace:
+RoR2.Projectile.ProjectileSteerTowardTarget.FixedUpdate () (at <da7c19fa62814b28bdb8f3a9223868e1>:IL_0000)
+
+Probably an error with Annihilator/Echo Elites. Error comes from `if (targetComponent.target)` on the first line, except it doesn't check if `targetComponent` exists, which is doesn't, then floods console with the nullref. Which is weird, since the component is supposed to exist on the projectile? Attempting fix by adding a nullcheck for `targetComponent`.
