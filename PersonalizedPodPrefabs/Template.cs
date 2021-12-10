61940078d4c69b37ef724235f167b20315110f85
DestroyedClone <destroyedclone@gmail.com>
2021-12-09 23:44:51 -0800

    Added Template

---
+++ PersonalizedPodPrefabs/Template.cs
@@ -0,0 +1,32 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using R2API.Utils;
+using RoR2;
+using System.Security;
+using System.Security.Permissions;
+using System.Collections.Generic;
+using UnityEngine;
+using R2API;
+using RoR2.Projectile;
+using static PersonalizedPodPrefabs.PersonalizePodPlugin;
+using UnityEngine.Networking;
+
+namespace PersonalizedPodPrefabs
+{
+    public class Template : PodBase
+    {
+        public override string BodyName => "Body";
+
+        public override GameObject CreatePod()
+        {
+            GameObject podPrefab = PrefabAPI.InstantiateClone(roboCratePodPrefab, PodPrefabName);
+            podPrefab.AddComponent<PodComponent>();
+            return podPrefab;
+        }
+
+        public class PodComponent : PodComponent
+        {
+
+        }
+    }
+}
