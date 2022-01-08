54037b14b1dd83bcbb5c9aebca3bc05288d11850
DestroyedClone <destroyedclone@gmail.com>
2022-01-07 21:44:31 -0800

    Used cleaner hooks, added pod destruction

--- UmbraLandInPods/Class1.cs
+++ UmbraLandInPods/Class1.cs
@@ -2,18 +2,9 @@
 using R2API;
 using R2API.Utils;
 using RoR2;
-using RoR2.UI;
-using System.Runtime.CompilerServices;
 using System.Security;
 using System.Security.Permissions;
-using TMPro;
 using UnityEngine;
-using UnityEngine.Networking;
-using RoR2.Artifacts;
-using static RoR2.Artifacts.DoppelgangerInvasionManager;
-using static RoR2.Artifacts.DoppelgangerSpawnCard;
-using System;
-using RoR2.Navigation;
 
 [module: UnverifiableCode]
 #pragma warning disable CS0618 // Type or member is obsolete
@@ -31,83 +22,74 @@ namespace UmbraLandInPods
     {
         public void Start()
         {
-            On.RoR2.Artifacts.DoppelgangerInvasionManager.CreateDoppelganger += DoppelgangerInvasionManager_CreateDoppelganger;
+            On.RoR2.CombatSquad.AddMember += CombatSquad_AddMember;
             On.EntityStates.SurvivorPod.Landed.OnEnter += ExitIfUmbra;
+            //RoR2.VehicleSeat.onPassengerExitGlobal += VehicleSeat_onPassengerExitGlobal;
+            On.EntityStates.SurvivorPod.ReleaseFinished.OnEnter += ReleaseFinished_OnEnter;
         }
+
+        private void ReleaseFinished_OnEnter(On.EntityStates.SurvivorPod.ReleaseFinished.orig_OnEnter orig, EntityStates.SurvivorPod.ReleaseFinished self)
+        {
+            orig(self);
+            var dot = self.gameObject.GetComponent<DestroyOnTimer>();
+            if (!dot)
+            {
+                dot = self.gameObject.AddComponent<DestroyOnTimer>();
+            }
+            dot.duration = 10f;
+        }
+
+        private void CombatSquad_AddMember(On.RoR2.CombatSquad.orig_AddMember orig, CombatSquad self, CharacterMaster memberMaster)
+        {
+            orig(self, memberMaster);
+            if (IsUmbra(memberMaster) && self.gameObject.name.StartsWith("ShadowCloneEncounter"))
+            {
+                Transform playerSpawnTransform = Stage.instance.GetPlayerSpawnTransform();
+                var body = memberMaster.GetBody();
+                Vector3 vector = body.footPosition;
+                Quaternion quaternion = Quaternion.identity;
+                if (playerSpawnTransform)
+                {
+                    vector = playerSpawnTransform.position;
+                    quaternion = playerSpawnTransform.rotation;
+                }
+                TeleportHelper.TeleportBody(body, vector);
+                Run.instance.HandlePlayerFirstEntryAnimation(body, vector, quaternion);
+                //if (memberMaster.bodyPrefab.GetComponent<CharacterBody>()?.preferredPodPrefab != null)
+            }
+        }
+
+        private bool IsUmbra(CharacterMaster characterMaster)
+        {
+            return characterMaster && characterMaster.inventory && characterMaster.inventory.GetItemCount(RoR2Content.Items.InvadingDoppelganger) > 0;
+        }
+
+        private bool IsUmbra(CharacterBody characterBody)
+        {
+            return characterBody && IsUmbra(characterBody.master);
+        }
+
+        private void VehicleSeat_onPassengerExitGlobal(VehicleSeat vehicleSeat, GameObject passenger)
+        {
+            var characterbody = passenger.GetComponent<CharacterBody>();
+            if (IsUmbra(characterbody))
+            {
+                characterbody.master?.GetComponent<RoR2.CharacterAI.BaseAI>()?.ForceAcquireNearestEnemyIfNoCurrentEnemy();
+            }
+        } // maybe?
 
         private void ExitIfUmbra(On.EntityStates.SurvivorPod.Landed.orig_OnEnter orig, EntityStates.SurvivorPod.Landed self)
         {
-			orig(self);
-			if (self.vehicleSeat)
+            orig(self);
+            if (self.vehicleSeat)
             {
-				var currentPassenger = self.vehicleSeat.currentPassengerBody;
-				if (currentPassenger && !currentPassenger.isPlayerControlled && currentPassenger.inventory && currentPassenger.inventory.GetItemCount(RoR2Content.Items.InvadingDoppelganger) > 0)
+                var currentPassenger = self.vehicleSeat.currentPassengerBody;
+                if (IsUmbra(currentPassenger))
                 {
-					bool? a = true;
-					self.HandleVehicleExitRequest(gameObject, ref a);
+                    bool? a = true;
+                    self.HandleVehicleExitRequest(gameObject, ref a);
                 }
-			}
+            }
         }
-
-        private void DoppelgangerInvasionManager_CreateDoppelganger(On.RoR2.Artifacts.DoppelgangerInvasionManager.orig_CreateDoppelganger orig, CharacterMaster srcCharacterMaster, Xoroshiro128Plus rng)
-        {
-			SpawnCard spawnCard = DoppelgangerSpawnCard.FromMaster(srcCharacterMaster);
-			if (!spawnCard)
-			{
-				return;
-			}
-			Transform playerSpawnTransform = Stage.instance.GetPlayerSpawnTransform();
-			DirectorCore.MonsterSpawnDistance input;
-			if (TeleporterInteraction.instance)
-			{
-				input = DirectorCore.MonsterSpawnDistance.Close;
-			}
-			else
-			{
-				input = DirectorCore.MonsterSpawnDistance.Far;
-			}
-			DirectorPlacementRule directorPlacementRule = new DirectorPlacementRule
-			{
-				spawnOnTarget = playerSpawnTransform,
-				placementMode = DirectorPlacementRule.PlacementMode.NearestNode
-			};
-			DirectorCore.GetMonsterSpawnDistance(input, out directorPlacementRule.minDistance, out directorPlacementRule.maxDistance);
-			DirectorSpawnRequest directorSpawnRequest = new DirectorSpawnRequest(spawnCard, directorPlacementRule, rng);
-			directorSpawnRequest.teamIndexOverride = new TeamIndex?(TeamIndex.Monster);
-			directorSpawnRequest.ignoreTeamMemberLimit = true;
-			CombatSquad combatSquad = null;
-			DirectorSpawnRequest directorSpawnRequest2 = directorSpawnRequest;
-			directorSpawnRequest2.onSpawnedServer = (Action<SpawnCard.SpawnResult>)Delegate.Combine(directorSpawnRequest2.onSpawnedServer, new Action<SpawnCard.SpawnResult>(delegate (SpawnCard.SpawnResult result)
-			{
-				if (!combatSquad)
-				{
-					combatSquad = UnityEngine.Object.Instantiate<GameObject>(Resources.Load<GameObject>("Prefabs/NetworkedObjects/Encounters/ShadowCloneEncounter")).GetComponent<CombatSquad>();
-				}
-				var spawnedInstance = result.spawnedInstance.GetComponent<CharacterMaster>();
-				combatSquad.AddMember(spawnedInstance);
-
-				Vector3 vector = Vector3.zero;
-				Quaternion quaternion = Quaternion.identity;
-				if (playerSpawnTransform)
-				{
-					vector = playerSpawnTransform.position;
-					quaternion = playerSpawnTransform.rotation;
-				}
-
-				var body = spawnedInstance.GetBody();
-				if (body.preferredPodPrefab)
-				{
-					GameObject gameObject = UnityEngine.Object.Instantiate<GameObject>(body.preferredPodPrefab, body.transform.position, quaternion);
-					gameObject.GetComponent<VehicleSeat>().AssignPassenger(body.gameObject);
-					NetworkServer.Spawn(gameObject);
-				}
-			}));
-			var spawnRequest = DirectorCore.instance.TrySpawnObject(directorSpawnRequest);
-			if (combatSquad)
-			{
-				NetworkServer.Spawn(combatSquad.gameObject);
-			}
-			UnityEngine.Object.Destroy(spawnCard);
-		}
     }
 }
