3186767ba9b351ca1d96a948c11bad74b256b285
DestroyedClone <destroyedclone@gmail.com>
2022-02-18 19:32:52 -0800

    mod

---
+++ BrotherPhaseSkip/Class1.cs
@@ -0,0 +1,58 @@
+﻿using BepInEx;
+using EntityStates.Missions.BrotherEncounter;
+using System.Security;
+using System.Security.Permissions;
+
+[module: UnverifiableCode]
+#pragma warning disable CS0618 // Type or member is obsolete
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+#pragma warning restore CS0618 // Type or member is obsolete
+
+namespace BrotherPhaseSkip
+{
+    [BepInPlugin("com.DestroyedClone.MithrixPhaseSkip", "Mithrix Phase Skip", "1.0.0")]
+    public class Class1 : BaseUnityPlugin
+    {
+        public void Start()
+        {
+            bool bind(int phaseNum, bool startingValue)
+            {
+                return Config.Bind("", $"Skip Phase {phaseNum}", startingValue).Value;
+            }
+
+            if (bind(1, false))
+            {
+                On.EntityStates.Missions.BrotherEncounter.Phase1.OnEnter += (orig, self) =>
+                {
+                    orig(self);
+                    self.PreEncounterBegin();
+                    self.outer.SetNextState(new Phase2());
+                };
+            }
+            if (bind(2, true))
+            {
+                On.EntityStates.Missions.BrotherEncounter.Phase2.OnEnter += (orig, self) =>
+                {
+                    orig(self);
+                    self.outer.SetNextState(new Phase3());
+                };
+            }
+            if (bind(3, false))
+            {
+                On.EntityStates.Missions.BrotherEncounter.Phase3.OnEnter += (orig, self) =>
+                {
+                    orig(self);
+                    self.outer.SetNextState(new Phase4());
+                };
+            }
+            if (bind(4, false))
+            {
+                On.EntityStates.Missions.BrotherEncounter.Phase4.OnEnter += (orig, self) =>
+                {
+                    orig(self);
+                    self.outer.SetNextState(new BossDeath());
+                };
+            }
+        }
+    }
+}
