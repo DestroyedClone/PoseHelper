2f382ee474d550a3a7bf72fce57a5676ac7be168
DestroyedClone <destroyedclone@gmail.com>
2022-04-29 06:40:46 -0700

    WIP, dunno

---
+++ ReducedVanillaDebugDotLogs/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using BepInEx;
+
+
+namespace ReducedVanillaDebugDotLogs
+{
+    [BepInPlugin("com.DestroyedClone.ReducedVanillaDebugDotLogs", "ReducedVanillaDebugDotLogs", "1.0")]
+    public class Class1 : BaseUnityPlugin
+    {
+        public void Awake()
+        {
+
+        }
+    }
+}
