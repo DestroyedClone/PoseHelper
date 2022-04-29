f8cb5d3df1a49d1ebf64a3a4b31b85c15b87456a
DestroyedClone <destroyedclone@gmail.com>
2022-04-29 06:37:32 -0700

    Added base

---
+++ ChestClaiming/Class1.cs
@@ -0,0 +1,59 @@
+﻿using BepInEx;
+using R2API;
+using R2API.Utils;
+using RoR2;
+using RoR2.Projectile;
+using System.Collections.Generic;
+using System.Security;
+using System.Security.Permissions;
+using UnityEngine;
+using System.Runtime.CompilerServices;
+
+[module: UnverifiableCode]
+#pragma warning disable CS0618 // Type or member is obsolete
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+#pragma warning restore CS0618 // Type or member is obsolete
+
+namespace ChestClaiming
+{
+    [BepInPlugin("com.DestroyedClone.ChestClaiming", "Chest Claiming", "1.0.0")]
+    [BepInDependency(R2API.R2API.PluginGUID, R2API.R2API.PluginVersion)]
+    [NetworkCompatibility(CompatibilityLevel.EveryoneMustHaveMod, VersionStrictness.DifferentModVersionsAreOk)]
+    [R2APISubmoduleDependency(nameof(EffectAPI), nameof(PrefabAPI))]
+    public class Class1 : BaseUnityPlugin
+    {
+        public void Start()
+        {
+            On.RoR2.ChestBehavior.ItemDrop += ChestBehavior_ItemDrop;
+            On.RoR2.GenericPickupController.BodyHasPickupPermission += GenericPickupController_BodyHasPickupPermission;
+        }
+
+        private bool GenericPickupController_BodyHasPickupPermission(On.RoR2.GenericPickupController.orig_BodyHasPickupPermission orig, CharacterBody body)
+        {
+            var original = orig(body);
+
+
+            return original;
+        }
+
+        private void ChestBehavior_ItemDrop(On.RoR2.ChestBehavior.orig_ItemDrop orig, ChestBehavior self)
+        {
+            orig(self);
+            var purchaseInteraction = self.GetComponent<PurchaseInteraction>();
+            if (purchaseInteraction)
+            {
+                var lastActivator = purchaseInteraction.lastActivator;
+                if (lastActivator)
+                {
+
+                }
+            }
+        }
+
+        public class ItemClaimInfo : MonoBehaviour
+        {
+            public CharacterMaster characterMaster;
+        }
+
+    }
+}
