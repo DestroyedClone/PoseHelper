0b3288b16929d4e89cd24b25222d1dbdb24aa278
DestroyedClone <DestroyedClone@gmail.com>
2022-09-22 14:12:18 -0700

    Moved ModCompat to its own file Added other related compat booleans

---
+++ AlternatePods/ModCompat.cs
@@ -0,0 +1,26 @@
+
+namespace AlternatePods
+{
+    public class ModCompat
+    {
+        //boysmell
+        public static bool enforcerInstalled = false;
+        public static bool minerInstalled = false;
+        public static bool chefInstalled = false;
+        public static bool paladinInstalled = false;
+        public static bool handInstalled = false;
+        public static bool sniperInstalled = false;
+        //timesweep
+        public static bool teslaTrooperInstalled = false;
+        public static bool regiInstalled = false;
+        public static bool henryInstalled = false;
+
+        public static bool starstormInstalled = false;
+
+        //team four dress
+        public static bool tf2ScoutInstalled = false;
+        public static bool tf2SoldierInstalled = false;
+        public static bool tf2MedicInstalled = false;
+        //
+    }
+}
